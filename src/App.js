01ba36348c70244536c3d3cb033bfd2d99aa662b
esradursun <escelik11@gmail.com>
2020-05-29 10:17:30 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import UserInput from './UserInput/UserInput';
+import UserOutput from './UserOutput/UserOutput';
 
-function App() {
-  return (
+class App extends Component {
+  state = {
+    userName:'Change Me'
+  }
+
+  userNameChangeHandler = (event) => {
+    this.setState({userName: event.target.value})
+  }
+
+
+  render() {
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hi, This is my First React App</h1>
+      <UserInput 
+        changed={this.userNameChangeHandler}
+        currentName={this.state.userName}/>
+      <UserOutput userName={this.state.userName}/>
     </div>
   );
+  }
+
 }
 
 export default App;
